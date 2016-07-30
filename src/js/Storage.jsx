b3984ad653a9105ae4ef8a373c23804a89738478
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-07-30 15:42:25 -0600

    event trigger succeed

--- src/js/Storage.jsx
+++ src/js/Storage.jsx
@@ -1,18 +1,39 @@
+MyComponents.Child = React.createClass({
+    render: function(){
+        return(
+            <div>Test</div>
+        )
+    }
+});
+
 class Storage extends React.Component{
 
-    handleChange(e) {
-        console.log("event triggering")
+    constructor(props) {
+        super(props);
+        this.state = {
+            childVisible: false,
+        };
     }
+
+    onChange(e) {
+        console.log("event triggering");
+        this.setState({childVisible: !this.state.childVisible});
+    }
+
     render(){
+
         return <div>
             <form>
                 <div className="inputWrap">
                     <a className="btn1" id="confirm">Upload</a>
                     <label className="btn1" htmlFor="files">Choose a file to upload</label>
-                    <input className="fileInput" type="file" id="files" name="files[]" onChange={this.handleChange} multiple />
+                    <input className="fileInput" type="file" id="files" name="files[]" onClick={this.onChange.bind(this)} multiple />
                 </div>
             </form>
-
+            {
+                this.state.childVisible?
+                <MyComponents.Child/>:null
+            }
             <div id="fileDropBox">
                 <output id="list"></output>
             </div>
