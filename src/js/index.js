1ef774990ca470b39fcda04fe9abdf1f69015851
PeicheTsao <peichetsao@gmail.com>
2016-08-09 02:42:26 -0600

    fixed logging refresh in index

--- src/js/index.js
+++ src/js/index.js
@@ -2,31 +2,29 @@
  * Created by pepe on 8/9/16.
  */
 var firebaseRef = new Firebase('https://coloringfun.firebaseio.com/');
-var user = JSON.parse(localStorage.getItem('amazingpixel::user'))
 var empty = false
 var svgId;
 var path;
-firebaseRef.child('users').child(user.uid).child('svgImgList').once('value', function (snapshot) {
-    if (snapshot.val() == null) {
-        empty = true
-        firebaseRef.child('svgTemplate').child('cat').once('value', function (snapshot) {
-            path = snapshot.val()
-            var newSvgImgRef = firebaseRef.child('svgColored').push()
-            svgId = newSvgImgRef.key()
-            firebaseRef.child('svgColored').child(svgId).child('svgInfo').set({});
-        })
-    }
-});
 $(document).ready(function () {
     $('#blockColoring').on('click', function () {
-        if (empty) {
-            firebaseRef.child('svgColored').child(svgId).child('svgInfo').set(path);
+        var user = JSON.parse(localStorage.getItem('amazingpixel::user'))
+        firebaseRef.child('users').child(user.uid).child('svgImgList').once('value', function (snapshot) {
+            if (snapshot.val() == null) {
+                empty = true
+                firebaseRef.child('svgTemplate').child('cat').once('value', function (snapshot) {
+                    path = snapshot.val()
+                    var newSvgImgRef = firebaseRef.child('svgColored').push()
+                    svgId = newSvgImgRef.key()
+                    firebaseRef.child('svgColored').child(svgId).child('svgInfo').set({});
+                    firebaseRef.child('svgColored').child(svgId).child('svgInfo').set(path);
+                    window.location = "blockColoring.html#" + svgId
+                })
+            } else {
 
-            window.location = "blockColoring.html#" + svgId
-        } else {
-            window.location = "blockGallery.html"
+                window.location = "blockGallery.html"
+            }
+        });
 
-        }
     })
-    
+
 })
